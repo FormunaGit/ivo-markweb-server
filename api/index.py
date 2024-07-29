721f451a562c331095b3837b0d3e62117e299abe
FormunaGit <formuna@tuta.io>
2024-07-29 12:39:54 -0400

    Added subpage support, now allowing people to make one website with multiple pages!

--- api/index.py
+++ api/index.py
@@ -3,18 +3,43 @@ import json
 
 app = Flask(__name__)
 
+def manager(pagename: str, subpage=None):
+    unparsedMetadata = open(f"raw/{pagename}/metadata.json").read()
+    metadata = json.loads(unparsedMetadata)
+
+    page = None
+    code = 200
+    if subpage == None:
+        try:
+            page = open(f"raw/{pagename}/{metadata['landPage']}").read()
+        except Exception as e:
+            page = 'Page does not exist'
+            code = 404
+    elif subpage != None:
+        try:
+            page = open(f"raw/{pagename}/{subpage}.md").read()
+        except Exception as e:
+            page = 'Subpage does not exist'
+            code = 404
+    else:
+        page = 'Error'
+
+    resp = make_response(page, code)
+    resp.mimetype = 'text/plain'
+    return resp
+
+
 @app.route('/')
 def home():
     return 'Hi :)'
 
-@app.route('/page/<pagename>')
+@app.route('/page/<pagename>') # Domain
 def getPage(pagename):
-    meda = open(f"raw/{pagename}/metadata.json").read()
-    metadata = json.loads(meda)
-    page = open(f"raw/{pagename}/{metadata['landPage']}").read()
-    resp = make_response(page, 200)
-    resp.mimetype = 'text/plain'
-    return resp
+    return manager(pagename)
+
+@app.route('/page/<pagename>/<subpage>') # Domain + Subpage
+def getSubpage(pagename, subpage):
+    return manager(pagename, subpage)
 
 @app.errorhandler(404)
 def page_not_found(e):
---
+++ raw/foobar.dev/developers.md
@@ -0,0 +1,3 @@
+# FooBar.DEV: Developer Documentation
+This page contains the developer documentation for FooBar.DEV.
+This page is also the first subpage on the Markweb! (The first *page* on the Markweb is the **FooBar.DEV** page.)
